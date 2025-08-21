47f040424f266f576cfc7a3be852684406608b96
Pablo Verde <pverde195@gmail.com>
2025-08-21 18:24:21 +0200

    Initial commit

---
+++ HuntressSkills/Skills/StalkingThePrey.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using BepInEx;
+using EntityStates;
+using R2API;
+using RoR2;
+using RoR2.Skills;
+using RoR2.Orbs;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using EntityStates.Bandit2;
+
+namespace HuntressSkills.Skills
+{
+    public class StalkingThePrey
+    {
+        public static BuffDef StalkingThePreyFirstHit; //buff that gives 100% crit chance and damage
+        public static float StalkingThePreyFirstHitMultBuff = 0.3f;
+
+
+        public static BuffDef PredatorFocus; //buff that gives crit chance and crit damage
+        public static float PredatorFocusCritMultDamage = 0.5f;
+        public static float PredatorFocusCritChance = 30f;
+
+        public static void Initialize(HuntressSkillsPlugin pluginInfo)
+        {
+            // First we must load our survivor's Body prefab. For this tutorial, we are making a skill for Commando
+            // If you would like to load a different survivor, you can find the key for their Body prefab at the following link
+            // https://xiaoxiao921.github.io/GithubActionCacheTest/assetPathsDump.html
+            GameObject huntressBodyPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Huntress/HuntressBody.prefab").WaitForCompletion();
+
+            LanguageAPI.Add(HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_UTILITY_STALKINGTHEPREY_NAME", "Stalking The Prey");
+            LanguageAPI.Add(HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_UTILITY_STALKINGTHEPREY_DESCRIPTION", $"Fire a boomerang for <style=cIsDamage>300% damage</style>.");
+
+            //Create the buffs we are going to do
+            CreateBuffs();
+
+            // Now we must create a SkillDef
+            SkillDef mySkillDef = ScriptableObject.CreateInstance<SkillDef>();
+
+            //Check step 2 for the code of the CustomSkillsTutorial.MyEntityStates.SimpleBulletAttack class
+            mySkillDef.activationState = new SerializableEntityStateType(typeof(StalkingThePreySkill));
+            mySkillDef.activationStateMachineName = "Body";
+            mySkillDef.baseMaxStock = 1;
+            mySkillDef.baseRechargeInterval = 7f;
+            mySkillDef.beginSkillCooldownOnSkillEnd = true;
+            mySkillDef.canceledFromSprinting = false;
+            mySkillDef.cancelSprintingOnActivation = false;
+            mySkillDef.fullRestockOnAssign = true;
+            mySkillDef.interruptPriority = InterruptPriority.Any;
+            mySkillDef.isCombatSkill = false;
+            mySkillDef.mustKeyPress = false;
+            mySkillDef.rechargeStock = 1;
+            mySkillDef.requiredStock = 1;
+            mySkillDef.stockToConsume = 1;
+            // For the skill icon, you will have to load a Sprite from your own AssetBundle
+            mySkillDef.icon = null;
+            mySkillDef.skillDescriptionToken = HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_UTILITY_STALKINGTHEPREY_DESCRIPTION";
+            mySkillDef.skillName = HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_UTILITY_STALKINGTHEPREY_NAME";
+            mySkillDef.skillNameToken = HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_UTILITY_STALKINGTHEPREY_NAME";
+
+            // This adds our skilldef. If you don't do this, the skill will not work.
+            ContentAddition.AddSkillDef(mySkillDef);
+
+            // Now we add our skill to one of the survivor's skill families
+            // You can change component.primary to component.secondary, component.utility and component.special
+            SkillLocator skillLocator = huntressBodyPrefab.GetComponent<SkillLocator>();
+            SkillFamily skillFamily = skillLocator.utility.skillFamily;
+
+            // If this is an alternate skill, use this code.
+            // Here, we add our skill as a variant to the existing Skill Family.
+            Array.Resize(ref skillFamily.variants, skillFamily.variants.Length + 1);
+            skillFamily.variants[skillFamily.variants.Length - 1] = new SkillFamily.Variant
+            {
+                skillDef = mySkillDef,
+                unlockableName = "",
+                viewableNode = new ViewablesCatalog.Node(mySkillDef.skillNameToken, false, null)
+            };
+        }
+
+
+        public static void StalkingThePreyFirstHitStatIncrease(CharacterBody characterBody, RecalculateStatsAPI.StatHookEventArgs args)
+        {
+            if (characterBody.HasBuff(StalkingThePreyFirstHit))
+            {
+                args.damageMultAdd += StalkingThePreyFirstHitMultBuff;
+                args.critAdd += 100f; 
+            }
+        }
+
+        public static void PredatorFocusStatIncrease(CharacterBody characterBody, RecalculateStatsAPI.StatHookEventArgs args)
+        {
+            if (characterBody.HasBuff(PredatorFocus))
+            {
+                args.critDamageMultAdd += PredatorFocusCritMultDamage;
+                args.critAdd += PredatorFocusCritChance;
+            }
+        }
+
+        public static void CreateBuffs()
+        {
+            //buff that gives 100% crit chance and damage
+            StalkingThePreyFirstHit = ScriptableObject.CreateInstance<BuffDef>();
+            StalkingThePreyFirstHit.name = "HuntressSkills: Stalking The Prey First Hit Buff";
+            StalkingThePreyFirstHit.buffColor = new Color(255, 255, 255);
+            StalkingThePreyFirstHit.isHidden = true;
+            StalkingThePreyFirstHit.canStack = false;
+            StalkingThePreyFirstHit.isDebuff = false;
+            //HuntressInstincts.iconSprite = MainAssets.LoadAsset<Sprite>("NailBombNailCooldownIcon.png");
+            ContentAddition.AddBuffDef(StalkingThePreyFirstHit);
+            RecalculateStatsAPI.GetStatCoefficients += StalkingThePreyFirstHitStatIncrease;
+
+            // another name FatalPrecision
+            PredatorFocus = ScriptableObject.CreateInstance<BuffDef>();
+            PredatorFocus.name = "HuntressSkills: Predator Focus Buff";
+            PredatorFocus.buffColor = new Color(255, 255, 255);
+            PredatorFocus.isHidden = false;
+            PredatorFocus.canStack = false;
+            PredatorFocus.isDebuff = false;
+            //PredatorFocus.iconSprite = MainAssets.LoadAsset<Sprite>("NailBombNailCooldownIcon.png");
+
+            ContentAddition.AddBuffDef(PredatorFocus);
+            RecalculateStatsAPI.GetStatCoefficients += PredatorFocusStatIncrease;
+        }
+
+        public class StalkingThePreySkill: BaseSkillState
+        {
+            public static float buffDuration = 5f;
+
+            public static string enterStealthSound = StealthMode.enterStealthSound;
+
+            public static string exitStealthSound = StealthMode.exitStealthSound;
+
+            private Animator animator;
+
+            protected Boolean skillUsedAfterInvis = false; //variable used for making next skill after invis deal more damage and the damage is buff not consumed for an instace of damage dealt before the skill is used //this will need some fix in the future to ensure only the skill damage is buffed
+
+            public override void OnEnter()
+            {
+                base.OnEnter();
+                animator = GetModelAnimator();
+                _ = (bool)animator;
+                if ((bool)base.characterBody)
+                {
+                    //add invis buff
+                    if (NetworkServer.active)
+                    {
+                        base.characterBody.AddBuff(RoR2Content.Buffs.Cloak);
+                        base.characterBody.AddBuff(RoR2Content.Buffs.CloakSpeed);
+                        base.characterBody.AddBuff(StalkingThePreyFirstHit);
+                        skillUsedAfterInvis = false;
+                    }
+
+                    //add a function to stop invis and add buff when user use a skill
+                    base.characterBody.onSkillActivatedAuthority += OnSkillActivatedRemoveInvis;
+                    //add a function to remove the first hit buff when an enemy recieve damage from the atacker
+                    On.RoR2.GlobalEventManager.OnHitEnemy += OnHitEnemyRemoveFirstHitBuff;
+
+
+                }
+                Util.PlaySound(enterStealthSound, base.gameObject);
+            }
+
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate();
+                outer.SetNextStateToMain();
+            }
+
+            public override void OnExit()
+            {
+                base.OnExit();
+            }
+
+            private void OnSkillActivatedRemoveInvis(GenericSkill skill)
+            {
+                Util.PlaySound(exitStealthSound, base.gameObject);
+                //animator.SetLayerWeight(animator.GetLayerIndex("Body, StealthWeapon"), 0f);
+
+                //when user atacks, then the invis buff is removed and a damage buff is applied
+                if (skill.skillDef.isCombatSkill)
+                {
+
+                    base.characterBody.RemoveBuff(RoR2Content.Buffs.CloakSpeed);
+                    base.characterBody.RemoveBuff(RoR2Content.Buffs.Cloak);
+
+                    base.characterBody.AddTimedBuff(PredatorFocus, buffDuration);
+                    skillUsedAfterInvis = true;
+
+                    //after that we remove the atack detection to be checked and reset the skill
+                    base.characterBody.onSkillActivatedAuthority -= OnSkillActivatedRemoveInvis;
+                }
+            }
+
+            private void OnHitEnemyRemoveFirstHitBuff(On.RoR2.GlobalEventManager.orig_OnHitEnemy orig, RoR2.GlobalEventManager self, RoR2.DamageInfo damageInfo, GameObject victim)
+            {
+                var attacker = damageInfo.attacker;
+                if (attacker & skillUsedAfterInvis) 
+                {
+                    var characterBody = attacker.GetComponent<CharacterBody>();
+                    if (characterBody.HasBuff(StalkingThePreyFirstHit)){
+                        characterBody.RemoveBuff(StalkingThePreyFirstHit);
+                    }
+                }
+            }
+
+            public override InterruptPriority GetMinimumInterruptPriority()
+            {
+                return InterruptPriority.Skill;
+            }
+        }
+    }
+}
---
+++ HuntressSkills/Skills/SwiftManeuver.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using BepInEx;
+using EntityStates;
+using R2API;
+using RoR2;
+using RoR2.Skills;
+using RoR2.Orbs;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using System.Linq;
+using EntityStates.Huntress;
+
+namespace HuntressSkills.Skills
+{
+    class SwiftManeuver
+    {
+        public static void Initialize(HuntressSkillsPlugin pluginInfo)
+        {
+            // First we must load our survivor's Body prefab. For this tutorial, we are making a skill for Commando
+            // If you would like to load a different survivor, you can find the key for their Body prefab at the following link
+            // https://xiaoxiao921.github.io/GithubActionCacheTest/assetPathsDump.html
+            GameObject huntressBodyPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Huntress/HuntressBody.prefab").WaitForCompletion();
+
+            LanguageAPI.Add(HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_SPECIAL_SWIFTMANEUVERSKILL_NAME", "Swift Maneuver");
+            LanguageAPI.Add(HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_SPECIAL_SWIFTMANEUVERSKILL_DESCRIPTION", $"Fire a boomerang for <style=cIsDamage>300% damage</style>.");
+
+
+            // Now we must create a SkillDef
+            SkillDef mySkillDef = ScriptableObject.CreateInstance<SkillDef>();
+
+            //Check step 2 for the code of the CustomSkillsTutorial.MyEntityStates.SimpleBulletAttack class
+            mySkillDef.activationState = new SerializableEntityStateType(typeof(SwiftManeuverSkill));
+            mySkillDef.activationStateMachineName = "Weapon";
+            mySkillDef.baseMaxStock = 1;
+            mySkillDef.baseRechargeInterval = 14f;
+            mySkillDef.beginSkillCooldownOnSkillEnd = true;
+            mySkillDef.canceledFromSprinting = false;
+            mySkillDef.cancelSprintingOnActivation = false;
+            mySkillDef.fullRestockOnAssign = true;
+            mySkillDef.interruptPriority = InterruptPriority.Any;
+            mySkillDef.isCombatSkill = true;
+            mySkillDef.mustKeyPress = false;
+            mySkillDef.rechargeStock = 1;
+            mySkillDef.requiredStock = 1;
+            mySkillDef.stockToConsume = 1;
+            // For the skill icon, you will have to load a Sprite from your own AssetBundle
+            mySkillDef.icon = null;
+            mySkillDef.skillDescriptionToken = HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_SPECIAL_SWIFTMANEUVERSKILL_DESCRIPTION";
+            mySkillDef.skillName = HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_SPECIAL_SWIFTMANEUVERSKILL_NAME  ";
+            mySkillDef.skillNameToken = HuntressSkillsPlugin.DEVELOPER_PREFIX + "HUNTRESS_SPECIAL_SWIFTMANEUVERSKILL_NAME";
+
+            // This adds our skilldef. If you don't do this, the skill will not work.
+            ContentAddition.AddSkillDef(mySkillDef);
+
+            // Now we add our skill to one of the survivor's skill families
+            // You can change component.primary to component.secondary, component.utility and component.special
+            SkillLocator skillLocator = huntressBodyPrefab.GetComponent<SkillLocator>();
+            SkillFamily skillFamily = skillLocator.special.skillFamily;
+
+            // If this is an alternate skill, use this code.
+            // Here, we add our skill as a variant to the existing Skill Family.
+            Array.Resize(ref skillFamily.variants, skillFamily.variants.Length + 1);
+            skillFamily.variants[skillFamily.variants.Length - 1] = new SkillFamily.Variant
+            {
+                skillDef = mySkillDef,
+                unlockableName = "",
+                viewableNode = new ViewablesCatalog.Node(mySkillDef.skillNameToken, false, null)
+            };
+        }
+
+        public class SwiftManeuverSkill : BaseSkillState
+        {
+            private Transform modelTransform;
+
+            public float firstBlinkDuration = 0.3f;
+
+            public float shotDuration;
+
+            public float secondBlinkDuration = 0.3f;
+
+            public float jumpCoefficient = 12f;
+
+            public static GameObject blinkPrefab = BaseBeginArrowBarrage.blinkPrefab;
+
+            public static string blinkSoundString = BaseBeginArrowBarrage.blinkSoundString;
+
+            [SerializeField]
+            public Vector3 blinkVector;
+
+            private Vector3 worldBlinkVector;
+
+            private float prepDuration;
+
+            private bool beginBlink;
+
+            private CharacterModel characterModel;
+
+            private HurtBoxGroup hurtboxGroup;
+
+            protected CameraTargetParams.AimRequest aimRequest;
+
+            public override void OnEnter()
+            {
+                base.OnEnter();
+                Util.PlaySound(blinkSoundString, base.gameObject);
+                modelTransform = GetModelTransform();
+                if ((bool)modelTransform)
+                {
+                    characterModel = modelTransform.GetComponent<CharacterModel>();
+                    hurtboxGroup = modelTransform.GetComponent<HurtBoxGroup>();
+                }
+                prepDuration = firstBlinkDuration / attackSpeedStat;
+                PlayAnimation("FullBody, Override", "BeginArrowRain", "BeginArrowRain.playbackRate", prepDuration);
+                if ((bool)base.characterMotor)
+                {
+                    base.characterMotor.velocity = Vector3.zero;
+                }
+                if ((bool)base.cameraTargetParams)
+                {
+                    aimRequest = base.cameraTargetParams.RequestAimType(CameraTargetParams.AimType.Aura);
+                }
+                Vector3 direction = GetAimRay().direction;
+                direction.y = 0f;
+                direction.Normalize();
+                Vector3 up = Vector3.up;
+                worldBlinkVector = Matrix4x4.TRS(base.transform.position, Util.QuaternionSafeLookRotation(direction, up), new Vector3(1f, 1f, 1f)).MultiplyPoint3x4(blinkVector) - base.transform.position;
+                worldBlinkVector.Normalize();
+            }
+
+            private void CreateBlinkEffect(Vector3 origin)
+            {
+                /*
+                EffectData effectData = new EffectData();
+                effectData.rotation = Util.QuaternionSafeLookRotation(worldBlinkVector);
+                effectData.origin = origin;
+                EffectManager.SpawnEffect(blinkPrefab, effectData, transmit: false);
+                */
+            }
+
+            public override void FixedUpdate()
+            {
+                base.FixedUpdate();
+                /*
+                if (base.fixedAge >= prepDuration && !beginBlink)
+                {
+                    beginBlink = true;
+                    CreateBlinkEffect(base.transform.position);
+                    if ((bool)characterModel)
+                    {
+                        characterModel.invisibilityCount++;
+                    }
+                    if ((bool)hurtboxGroup)
+                    {
+                        HurtBoxGroup hurtBoxGroup = hurtboxGroup;
+                        int hurtBoxesDeactivatorCounter = hurtBoxGroup.hurtBoxesDeactivatorCounter + 1;
+                        hurtBoxGroup.hurtBoxesDeactivatorCounter = hurtBoxesDeactivatorCounter;
+                    }
+                }
+                if (beginBlink && (bool)base.characterMotor)
+                {
+                    base.characterMotor.velocity = Vector3.zero;
+                    base.characterMotor.rootMotion += worldBlinkVector * (base.characterBody.jumpPower * jumpCoefficient * Time.fixedDeltaTime);
+                }
+                if (base.fixedAge >= shotDuration + prepDuration && base.isAuthority)
+                {
+                    outer.SetNextState(new AimArrowSnipe());
+                }
+                */
+            }
+
+            public override void OnExit()
+            {
+                /*
+                CreateBlinkEffect(base.transform.position);
+                modelTransform = GetModelTransform();
+                if ((bool)modelTransform)
+                {
+                    TemporaryOverlay temporaryOverlay = modelTransform.gameObject.AddComponent<TemporaryOverlay>();
+                    temporaryOverlay.duration = 0.6f;
+                    temporaryOverlay.animateShaderAlpha = true;
+                    temporaryOverlay.alphaCurve = AnimationCurve.EaseInOut(0f, 1f, 1f, 0f);
+                    temporaryOverlay.destroyComponentOnEnd = true;
+                    temporaryOverlay.originalMaterial = LegacyResourcesAPI.Load<Material>("Materials/matHuntressFlashBright");
+                    temporaryOverlay.AddToCharacerModel(modelTransform.GetComponent<CharacterModel>());
+                    TemporaryOverlay temporaryOverlay2 = modelTransform.gameObject.AddComponent<TemporaryOverlay>();
+                    temporaryOverlay2.duration = 0.7f;
+                    temporaryOverlay2.animateShaderAlpha = true;
+                    temporaryOverlay2.alphaCurve = AnimationCurve.EaseInOut(0f, 1f, 1f, 0f);
+                    temporaryOverlay2.destroyComponentOnEnd = true;
+                    temporaryOverlay2.originalMaterial = LegacyResourcesAPI.Load<Material>("Materials/matHuntressFlashExpanded");
+                    temporaryOverlay2.AddToCharacerModel(modelTransform.GetComponent<CharacterModel>());
+                }
+                if ((bool)characterModel)
+                {
+                    characterModel.invisibilityCount--;
+                }
+                if ((bool)hurtboxGroup)
+                {
+                    HurtBoxGroup hurtBoxGroup = hurtboxGroup;
+                    int hurtBoxesDeactivatorCounter = hurtBoxGroup.hurtBoxesDeactivatorCounter - 1;
+                    hurtBoxGroup.hurtBoxesDeactivatorCounter = hurtBoxesDeactivatorCounter;
+                }
+                aimRequest?.Dispose();
+                */
+                base.OnExit();
+            }
+        }
+    }
+}
